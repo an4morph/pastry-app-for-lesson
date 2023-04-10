6b6b0adf4c057cfe33da59837bbebf2d8146c24b
an4morph <avgustin456@gmail.com>
2023-04-10 12:14:11 +0600

    add api logic for pastry item

--- js/creators.js
+++ js/creators.js
@@ -2,9 +2,14 @@ export function createCatalogItem(item) {
   const itemContainer = document.createElement('div')
   itemContainer.className = 'catalog__item'
 
+  const link = document.createElement('a')
+  link.href = `http://127.0.0.1:5500/item.html?id=${item.id}`
+
   const title = document.createElement('h2')
   title.className = 'catalog__item-title'
   title.textContent = item.name
+
+  link.append(title)
 
   const image = document.createElement('img')
   image.setAttribute('src', item.image)
@@ -18,7 +23,7 @@ export function createCatalogItem(item) {
   buyBtn.className = 'catalog__item-buy-btn'
   buyBtn.textContent = 'Buy'
 
-  itemContainer.append(title, image, price, buyBtn)
+  itemContainer.append(link, image, price, buyBtn)
 
   return itemContainer
 }
--- js/item.js
+++ js/item.js
@@ -1,1 +1,51 @@
-console.log('item page')
+const api = 'http://localhost:1717'
+
+async function getPastryItem(id) {
+  const response = await fetch(`${api}/pastry/detail/${id}`)
+  const data = await response.json()
+  return data
+}
+
+const createTableRow = (key, value) => {
+  if (['id', 'name', 'image'].includes(key)) return
+
+  const tr = document.createElement('tr')
+  const td1 = document.createElement('td')
+  const td2 = document.createElement('td')
+
+  if (key === 'cost') {
+    td1.textContent = 'Price'
+    td2.textContent = `$${value}`
+  } else if (key === 'inStock') {
+    td1.textContent = 'In Stock'
+    td2.textContent = value
+  } else if (key === 'ingredients') {
+    td1.textContent = 'Ingredients'
+    td2.textContent = value.join(', ')
+  } else if (key === 'hasDelivery') {
+    td1.textContent = 'Delivery'
+    td2.textContent = value ? 'yes' : 'no'
+  } else if (key === 'address') {
+    td1.textContent = 'Address'
+    td2.textContent = `${value.street} ${value.houseNumber}`
+  }
+  
+  tr.append(td1, td2)
+  return tr
+}
+
+async function renderPastryItem() {
+  const pastryId = new URLSearchParams(window.location.search).get('id')
+  const data = await getPastryItem(pastryId)
+
+  document.querySelector('.pastry-item__title').textContent = data.name
+  document.querySelector('.pastry-item__image').src = data.image
+  const tbody = document.querySelector('.pastry-item__table tbody')
+  
+  for (let key in data) {
+    const tr = createTableRow(key, data[key])
+    if (tr) tbody.append(tr)
+  }
+}
+
+renderPastryItem()
